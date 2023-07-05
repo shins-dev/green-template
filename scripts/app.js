ea7e12d044f6f4d87ea0a147b731a182e169fc90
shins-dev <hong.vo.anothames@gmail.com>
2023-07-05 11:32:44 +0700

    fix-login

--- scripts/app.js
+++ scripts/app.js
@@ -1,38 +1,54 @@
-const icofontErrors = document.querySelectorAll(".text-icon-error");
-
-if (icofontErrors) {
-  icofontErrors.forEach((icofontError) => {
-    icofontError.addEventListener("mouseenter", () => {
-      icofontError.style.fontSize = "32px";
-      icofontError.style.color = "red";
-      icofontError.style.cursor = "pointer";
-      icofontError.style.transition = "color 0.3s ease";
-    });
-
-    icofontError.addEventListener("mouseleave", () => {
-      icofontError.style.color = "";
-      icofontError.style.cursor = "";
-      icofontError.style.fontSize = "";
-    });
-  });
-}
 window.addEventListener("load", () => {
-  const toggle = document.querySelector(".header-outhor-resgiter");
-  const menu = document.querySelector(".login");
+  const icofontErrors = document.querySelectorAll(".text-icon-error");
+  if (icofontErrors) {
+    icofontErrors.forEach((icofontError) => {
+      icofontError.addEventListener("mouseenter", () => {
+        icofontError.style.fontSize = "32px";
+        icofontError.style.color = "red";
+        icofontError.style.cursor = "pointer";
+        icofontError.style.transition = "color 0.3s ease";
+      });
+
+      icofontError.addEventListener("mouseleave", () => {
+        icofontError.style.color = "";
+        icofontError.style.cursor = "";
+        icofontError.style.fontSize = "";
+      });
+    });
+  }
+  const toggleLogin = document.querySelector(".logins");
+  const menuLogin = document.querySelector(".log-in");
   const body = document.querySelector("body");
 
-  toggle?.addEventListener("click", () => {
-    menu?.classList.add("is-show");
-    body.style.overflow = "hidden"; // Ngăn chặn việc lăn con chuột khi menu được mở ra
+  toggleLogin?.addEventListener("click", () => {
+    menuLogin?.classList.add("is-show");
+    body.style.overflow = "hidden";
   });
 
   document.addEventListener("click", (e) => {
     if (
-      e.target.matches(".login, .login *") &&
+      e.target.matches(".log-in, .log-in *") &&
       e.target.matches(".icofont-error")
     ) {
-      menu?.classList.remove("is-show");
-      body.style.overflow = ""; // Cho phép lăn con chuột khi menu bị đóng lại
+      menuLogin?.classList.remove("is-show");
+      body.style.overflow = "";
+    }
+  });
+
+  const toggleSignup = document.querySelector(".singups");
+  const menuLogout = document.querySelector(".sign-in");
+  toggleSignup?.addEventListener("click", () => {
+    menuLogout?.classList.add("is-show");
+    body.style.overflow = "hidden";
+  });
+
+  document.addEventListener("click", (e) => {
+    if (
+      e.target.matches(".sign-in, .sign-in *") &&
+      e.target.matches(".icofont-error")
+    ) {
+      menuLogout?.classList.remove("is-show");
+      body.style.overflow = "";
     }
   });
 });
